f5eadf97d3a946bb4144d66f8700e38c01f5c6f7
kaizerslawten <antonio268sic2@gmail.com>
2017-06-19 17:00:03 -0300

    Antonio Rosario - seg, 19 de jun de 2017 17:00:02

--- timer.sh
+++ timer.sh
@@ -2,22 +2,22 @@ echo Sincronismo de Repositório automatico Iniciado $(date)
 while true; do
 	sleep 2;
 	#Efetuando commit para master 12:00
-	if [[ $(date) == *"12:00"* ]]; then
-	  git add . && git commit -m "'Antoio Rosario - '$(date)" && git push origin master > $1 >> /dev/null
+	if [[ $(date) == *"12:00:"* ]]; then
+	  git add . && git commit -m "Antonio Rosario - $(date)" && git push origin master > $1 >> /dev/null
 	  echo Commit Automatico efetuado $(date) com sucesso!
 	  sleep 60;
 	fi
 	
 	#Efetuando commit para master 15:00
-	if [[ $(date) == *"15:00"* ]]; then
-		git add . && git commit -m "'Antoio Rosario - '$(date)" && git push origin master >> /dev/null
+	if [[ $(date) == *"15:00:"* ]]; then
+		git add . && git commit -m "Antonio Rosario - $(date)" && git push origin master >> /dev/null
 		echo Commit Automatico efetuado $(date) com sucesso!
 		sleep 60;
 	fi
 	
 	#Efetuando commit para master 17:00
-	if [[ $(date) == *"17:00"* ]]; then	
-		git add . && git commit -m "Antoio Rosario - $(date)" && git push origin master >> /dev/null
+	if [[ $(date) == *"17:00:"* ]]; then	
+		git add . && git commit -m "Antonio Rosario - $(date)" && git push origin master >> /dev/null
 		echo Commit Automatico efetuado $(date) com sucesso!
 		sleep 60;
 		fi
