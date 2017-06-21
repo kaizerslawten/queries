86d4fec565abdecee9f9175cdea2200fd935ebb1
kaizerslawten <antonio268sic2@gmail.com>
2017-06-21 15:00:02 -0300

    Antonio Rosario - qua, 21 de jun de 2017 15:00:01

--- timer.sh
+++ timer.sh
@@ -3,28 +3,28 @@ while true; do
 	sleep 2;
 	#Efetuando commit para master 12:00
 	if [[ $(date) == *"12:00:"* ]]; then
-	  git add . && git commit -m "Antonio Rosario - $(date)" && git push origin master > $1 >> /dev/null
+	  git add . && git commit -m "Antonio Rosario - $(date)" && git push origin master
 	  echo Commit Automatico efetuado $(date) com sucesso!
 	  sleep 60;
 	fi
 	
 	#Efetuando commit para master 15:00
 	if [[ $(date) == *"15:00:"* ]]; then
-		git add . && git commit -m "Antonio Rosario - $(date)" && git push origin master >> /dev/null
+		git add . && git commit -m "Antonio Rosario - $(date)" && git push origin master
 		echo Commit Automatico efetuado $(date) com sucesso!
 		sleep 60;
 	fi
 	
 	#Efetuando commit para master 17:00
 	if [[ $(date) == *"17:00:"* ]]; then	
-		git add . && git commit -m "Antonio Rosario - $(date)" && git push origin master >> /dev/null
+		git add . && git commit -m "Antonio Rosario - $(date)" && git push origin master
 		echo Commit Automatico efetuado $(date) com sucesso!
 		sleep 60;
 		fi
 		
 		#Efetuando commit para master 18:00
 	if [[ $(date) == *"18:00:"* ]]; then	
-		git add . && git commit -m "Antonio Rosario - $(date)" && git push origin master >> /dev/null
+		git add . && git commit -m "Antonio Rosario - $(date)" && git push origin master
 		echo Commit Automatico efetuado $(date) com sucesso!
 		sleep 60;
 		fi
